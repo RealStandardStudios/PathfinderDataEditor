abdb343b7539a3ccc5fa767ddef7b867647c2580
Josh <the-cheese@hotmail.com>
2014-07-31 15:17:36 +1200

    Added items partial and partials for that partial

--- src/view/partials/ItemsController.java
+++ src/view/partials/ItemsController.java
@@ -1,13 +1,35 @@
 package view.partials;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import jefXif.LoadablePartials;
+import jefXif.Strings;
 import jefXif.WindowController;
 
-public class ItemsController extends WindowController{
+public class ItemsController extends WindowController implements LoadablePartials{
 
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
+		try {
+			loadPartial("BasicGoods");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+	
+	@Override
+	public Node loadPartial(String name) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(this.getClass().getResource(
+				Strings.FileLoc+ "itemPartials/" + name + "Partial.fxml"));
+		
+		AnchorPane partial = loader.load();
+		return partial;
 	}
 
 }
