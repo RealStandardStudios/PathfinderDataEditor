a0de31591b5fff6290fc93245cc7a7455e9c13ad
Doslidnyk <ian.s.larsen@gmail.com>
2014-08-05 17:01:01 +1200

    The changes ian made?

--- src/view/partials/ClassesController.java
+++ src/view/partials/ClassesController.java
@@ -6,25 +6,92 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import jefXif.WindowController;
-
 import pathfinder.data.Classes.Class;
+import pathfinder.data.Classes.Druid;
 
 public class ClassesController extends WindowController{
 	
 	@FXML
-	private TableView<Class> tableClass;
+	private TableView<Class> tableClasses;
 	
 	@FXML
 	private TableColumn<Class, String> columnClassName;
+	
+	@FXML
+	private Label lblDescription;
+	
+	@FXML
+	private Label lblRole;
+	
+	@FXML
+	private Label lblAlignments;
+	
+	@FXML
+	private Label lblHitDice;
+	
+	@FXML
+	private Label lblClassSkills;
+	
+	@FXML
+	private Label lblSkillRanksPerLevel;
+	
+	@FXML
+	private Label lblWeaponProf;
+	
+	@FXML
+	private Label lblArmorProf;
+	
+	@FXML
+	private Label lblStartingWealthD6;
+	
+	private ObservableList<Class> obsListClasses = FXCollections.observableArrayList();
 
+	/**
+	 * Return data as Observable List of Class
+	 * @return
+	 */
+	public ObservableList<Class> getObsListClasses() {
+		return obsListClasses;
+	}
+	
 	@Override
 	public void initialize() {
 		// TODO Auto-generated method stub
+		
+		columnClassName.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
+		
+		// Dummy test data
+		obsListClasses.add(new Druid("Water bender", "Bends water somehow.", "Bends water, of course.", 9001, null, null, 2, 30, null, null, null, null, null, null));
+		
+		tableClasses.setItems(obsListClasses);
+		
 		readSummary();
+	}
+	
+	/**
+	 * Populate Class Details labels with data from the selected Class
+	 * 
+	 * @param c : selected Class
+	 */
+	private void showClassDetails(Class c) {
+		if(c != null) {
+			lblDescription.setText(c.getName());
+			lblRole.setText(c.getRole());
+			lblAlignments.setText(c.getAlignments());
+			lblHitDice.setText(c.getHitDice().toString());
+			lblClassSkills.setText(c.getClassSkillsToString());
+			lblSkillRanksPerLevel.setText(c.getSkillRanksToString().toString());
+			lblWeaponProf.setText(c.getWeaponProfsToString());
+			lblArmorProf.setText(c.getArmorProfsToString());
+			lblStartingWealthD6.setText(c.getStartingWealthToString().toString());
+		}
 	}
 	
 	/**
