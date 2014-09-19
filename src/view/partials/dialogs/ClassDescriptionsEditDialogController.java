ae92e584b48ff8f310abbb6466c14521e0a85279
Matthew <lololirpro@gmail.com>
2014-09-19 15:14:54 -0700

    Compleated all of the edit class systems, bar the monk table.. fuck the monk table. Saving classes is borked

--- src/view/partials/dialogs/ClassDescriptionsEditDialogController.java
+++ src/view/partials/dialogs/ClassDescriptionsEditDialogController.java
@@ -1,5 +1,7 @@
 package view.partials.dialogs;
 
+import java.util.ArrayList;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,6 +12,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import jefXif.DialogController;
 import pathfinder.data.DiceType;
+import pathfinder.data.Character.Alignment;
 import pathfinder.data.Classes.Class;
 
 public class ClassDescriptionsEditDialogController extends DialogController {
@@ -85,7 +88,39 @@ public class ClassDescriptionsEditDialogController extends DialogController {
 		txtStartingWealth.setText(Integer.toString(pClass.getStartingWealthD6()));
 		cboHitDice.setValue(pClass.getHitDice());
 		if(pClass.getRequireAlignments()!=null) {
-			
+			for (Alignment alignment : pClass.getRequireAlignments()) {
+				switch (alignment) {
+				case ChaoticEvil:
+					cbChaoticEvil.setSelected(true);
+					break;
+				case ChaoticNeutral:
+					cbChaoticNeutral.setSelected(true);
+					break;
+				case ChaoticGood:
+					cbChaoticGood.setSelected(true);
+					break;
+				case LawfulEvil:
+					cbLawfulEvil.setSelected(true);
+					break;
+				case LawfulGood:
+					cbLawfulGood.setSelected(true);
+					break;
+				case LawfulNeutral:
+					cbLawfulNeutral.setSelected(true);
+					break;
+				case Neutral:
+					cbTrueNeutral.setSelected(true);
+					break;
+				case NeutralEvil:
+					cbNeutralEvil.setSelected(true);
+					break;
+				case NeutralGood:
+					cbNeutralGood.setSelected(true);
+					break;
+				default:
+					break;
+				}
+			}
 		}
 	}
 
@@ -99,6 +134,17 @@ public class ClassDescriptionsEditDialogController extends DialogController {
 		pClass.setArmorProficiencies(txtaArmorProficiency.getText().split(","));
 		pClass.setWeaponProficiencies(txtaWeaponProficiency.getText().split(","));
 		pClass.setHitDice(cboHitDice.getValue());
+		ArrayList<Alignment> alignments = new ArrayList<Alignment>();
+		if(cbChaoticEvil.selectedProperty().get()) alignments.add(Alignment.ChaoticEvil);
+		if(cbChaoticGood.selectedProperty().get()) alignments.add(Alignment.ChaoticGood);
+		if(cbChaoticNeutral.selectedProperty().get()) alignments.add(Alignment.ChaoticNeutral);
+		if(cbLawfulEvil.selectedProperty().get()) alignments.add(Alignment.LawfulEvil);
+		if(cbLawfulGood.selectedProperty().get()) alignments.add(Alignment.LawfulGood);
+		if(cbLawfulNeutral.selectedProperty().get()) alignments.add(Alignment.LawfulNeutral);
+		if(cbNeutralEvil.selectedProperty().get()) alignments.add(Alignment.NeutralEvil);
+		if(cbNeutralGood.selectedProperty().get()) alignments.add(Alignment.NeutralGood);
+		if(cbTrueNeutral.selectedProperty().get()) alignments.add(Alignment.Neutral);
+		pClass.setRequireAlignments(alignments.toArray(new Alignment[alignments.size()]));
 		okayClicked = true;
 		this.getDialogStage().close();
 	}
--- src/view/partials/dialogs/ClassFeatureEditDialogController.java
+++ src/view/partials/dialogs/ClassFeatureEditDialogController.java
@@ -3,6 +3,10 @@ package view.partials.dialogs;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.controlsfx.dialog.Dialogs;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,21 +21,23 @@ import jefXif.DialogController;
 import jefXif.PartialLoader;
 import jefXif.WindowController;
 import pathfinder.data.Classes.Objects.Feature;
+import pathfinder.data.Effects.Effect;
 import view.partials.dialogs.effectDialogPartials.EffectPartialController;
 
-public class ClassFeatureEditDialogController extends DialogController implements PartialLoader{
-	
-	String[] partials = {"MiscEffect", "ArmorClassEffect",
+public class ClassFeatureEditDialogController extends DialogController
+		implements PartialLoader {
+
+	String[] partials = { "MiscEffect", "ArmorClassEffect",
 			"CombatManeuverBonusEffect", "InitiativeEffect",
 			"SaveAttributeEffect", "SkillEffect", "ActionEffect",
 			"ActionToFreeEffect", "ActionToSwiftEffect", "ActionToMoveEffect",
 			"FeintActionEffect", "MiscEffect", "OnCritEffect", "DamageEffect",
-			"DamageMultiplierEffect", "ItemCreationEffect", "MetaMagicEffect"};
-	
+			"DamageMultiplierEffect", "ItemCreationEffect", "MetaMagicEffect" };
+
 	ObservableList<String> effects;
 	HashMap<String, EffectPartialController> effectPartials;
 	Feature feature;
-	
+
 	@FXML
 	TextField txtFeatureName;
 	@FXML
@@ -42,14 +48,14 @@ public class ClassFeatureEditDialogController extends DialogController implement
 	AnchorPane EffectPartialPane;
 	@FXML
 	ComboBox<String> cboEffect;
-	
+
 	public ClassFeatureEditDialogController() {
 		effects = FXCollections.observableArrayList();
 		for (String s : partials) {
 			effects.add(s);
 		}
 	}
-	
+
 	@Override
 	public void initialize() {
 		cboEffect.setItems(effects);
@@ -59,12 +65,16 @@ public class ClassFeatureEditDialogController extends DialogController implement
 				this.effectPartials.put(string,
 						(EffectPartialController) loadPartial(string));
 			} catch (IOException e) {
-				//Logger.getLogger(getClass()).log(Level.SEVERE, e.getMessage());
+				Dialogs.create().title("Error")
+						.masthead("There was an error making this dialog")
+						.message(e.getMessage());
+				// Logger.getLogger(getClass()).log(Level.SEVERE,
+				// e.getMessage());
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	private WindowController loadPartial(String name) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(this.getClass().getResource(
@@ -75,27 +85,70 @@ public class ClassFeatureEditDialogController extends DialogController implement
 		controller.setNode(node);
 		return controller;
 	}
-	
+
 	@FXML
 	private void handleChangedEffect(ActionEvent event) {
 		if (cboEffect.getValue() != "" || !cboEffect.getValue().equals(null))
-			EffectPartialPane.getChildren().setAll(
-					effectPartials.get(cboEffect.getValue()).getNode());
+			EffectPartialPane.getChildren().setAll(effectPartials.get(cboEffect.getValue()).getNode());
+		if (feature.getEffectProperty().get() != null)
+			effectPartials.get(cboEffect.getValue()).setEffect(feature.getEffectProperty().get());
+		else {
+			// feature.getEffectProperty().set();
+			Effect effect = null;
+			try {
+				effect = (Effect) Class.forName(
+						"pathfinder.data.Effects." + cboEffect.getValue())
+						.newInstance();
+			} catch (ClassNotFoundException e) {
+				try {
+					effect = (Effect) Class.forName(
+							"pathfinder.data.Effects.Actions."
+									+ cboEffect.getValue()).newInstance();
+				} catch (InstantiationException | IllegalAccessException e1) {
+					e1.printStackTrace();
+				} catch (ClassNotFoundException e1) {
+					try {
+						effect = (Effect) Class.forName(
+								"pathfinder.data.Effects.NonValued."
+										+ cboEffect.getValue()).newInstance();
+					} catch (InstantiationException | IllegalAccessException
+							| ClassNotFoundException e2) {
+						e2.printStackTrace();
+					}
+				}
+			} catch (InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+			feature.setEffect(effect);
+			effectPartials.get(cboEffect.getValue()).setEffect(feature.getEffectProperty().get());
+		}
 	}
-	
+
 	@FXML
 	public void handleOkay(ActionEvent event) {
-		
+		feature.setDescription(txtaFeatureDescription.getText());
+		feature.setType(txtFeatureType.getText());
+		feature.getNameProperty().set(txtFeatureName.getText());
+		feature.setEffect(effectPartials.get(
+				cboEffect.getSelectionModel().getSelectedItem()).getEffect());
+		this.getDialogStage().close();
 	}
 
 	public void setFeature(Feature selectedFeature) {
-		// TODO Auto-generated method stub
-		
+		this.feature = selectedFeature;
+		txtFeatureName.setText(feature.getNameProperty().get());
+		txtFeatureType.setText(feature.getTypeProperty().get());
+		txtaFeatureDescription.setText(feature.getDescriptionProperty().get());
+		if (feature.getEffectProperty().get() != null)
+			cboEffect.getSelectionModel().select(
+					feature.getEffectProperty().get().getName());
+		else {
+			cboEffect.getSelectionModel().select("MiscEffect");
+		}
 	}
 
 	public Feature getFeature() {
-		// TODO Auto-generated method stub
-		return null;
+		return feature;
 	}
-	
+
 }
